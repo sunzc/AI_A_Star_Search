50b5ded53ecb02343d241866b0353554574bc99d
Zhichuang Sun <zhisun@cs.stonybrook.edu>
2016-10-09 12:33:27 -0400

    extend the timeout to 60s
    Signed-off-by: Zhichuang Sun <zhisun@cs.stonybrook.edu>

--- test.sh
+++ test.sh
@@ -7,9 +7,9 @@ do
 	echo $N
 	RETVAL=124
 	until [ $RETVAL -eq 0 ]; do
-		python3 ./puzzleGenerator.py 4 20 output_$N.txt
-		timeout 30 ./puzzleSolver.py 1 4 output_$N.txt output_$N.res_h2 2
+		python3 ./puzzleGenerator.py 4 output_$N.txt
+		timeout 60 ./puzzleSolver.py 2 4 output_$N.txt output_$N.res_h2
 		RETVAL=$?
 	done
-	./puzzleSolver.py 1 4 output_$N.txt output_$N.res_h1 1
+	./puzzleSolver.py 2 4 output_$N.txt output_$N.res_h2
 done
